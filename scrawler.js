aa7a3dbc1b2cbbe266f612768289b34379e6bfe6
Toan NGUYEN <toan.nguyen@s60.com.au>
2016-10-03 23:01:16 +0700

    test crawler

--- scrawler.js
+++ scrawler.js
@@ -2,13 +2,17 @@ var Crawler = require("simplecrawler"),
     cheerio = require('cheerio'),
     fs = require('fs');
 
-var crawler = new Crawler("http://localhost:3000/");
+var crawler = new Crawler("http://vnexpress.net/photo/thoi-su/hien-truong-xe-taxi-no-tung-tren-duong-3478068.html");
 
 crawler.on("fetchcomplete", function(queueItem, responseBuffer, response) {
     // console.log("I just received %s (%d bytes)", queueItem.url, responseBuffer.length);
     // console.log("It was a resource of type %s", response.headers['content-type']);
 
-    console.log('fetchcomplete %s', queueItem.url);
+    var $ = cheerio.load(responseBuffer);
+    var result = '';
+    result += 'title: ' + $('.title_news h1').html();
+    result += '\n content: ' + $('#article_content').html();
+    fs.writeFileSync('result.txt' , result, 'utf8');
 });
 
 // crawler.discoverResources = function(buffer, queueItem) {
@@ -33,6 +37,15 @@ crawler.on('fetchstart', function(queueItem, resources) {
 //        })
 //    }
 // });
+//
+
+// crawler.discoverResources = function(buffer, queueItem) {
+//     var $ = cheerio.load(buffer.toString("utf8"));
+
+//     return $("a[href]").map(function () {
+//         return $(this).attr("href");
+//     }).get();
+// };
 
 crawler.on('discoverycomplete', function(queueItem, resources) {
    console.log('discoverycomplete %s', queueItem.url);
@@ -50,8 +63,8 @@ crawler.on('complete', function() {
 
 crawler.interval = 10000; // Ten seconds
 crawler.maxConcurrency = 3;
-
-//crawler.maxDepth = 2; // First page and discovered links from it are fetched
+crawler.maxDepth = 1; // First page and discovered links from it are fetched
+crawler.decodeResponses=true;
 
 
 crawler.start();
